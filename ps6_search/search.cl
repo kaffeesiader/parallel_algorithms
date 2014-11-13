9b79ac9d89dc2eb999b4d0c3b05544bbaf885ece
Martin Roland Griesser <csap2497@zid-t9pc2017.uibk.ac.at>
2014-11-13 16:51:02 +0100

    Commit

--- ps6_search/search.cl
+++ ps6_search/search.cl
@@ -1,38 +1,39 @@
 #pragma OPENCL EXTENSION cl_khr_byte_addressable_store : enable
 // #pragma OPENCL EXTENSION cl_khr_fp64: enable
 
-__kernel void search(__global VALUE *data, __global int *g_result, VALUE epsilon, VALUE value,
+__kernel void search(__global VALUE *data, __global int *result, VALUE epsilon, VALUE value,
 		     int length, __local int *l_result)
 {
-    int lId = get_local_id(0);
-    int l_size = get_local_size(0);
-    int lower = lId * l_size;
-    int upper = lower + l_size;
+	int lId = get_local_id(0);
+	int l_size = get_local_size(0);
 
-    if(upper > length) {
-	upper = length;
-    }
+	int lower = lId * l_size;
+	int upper = lower + l_size;
 
-    l_result[lId] = (int)-1;
-
-    for(int gId = lower; gId < upper; gId++) {
-	VALUE mine = data[gId];
-	if(fabs(mine - value) < epsilon) {
-	    l_result[lId] = get_global_id(0);
-	    break;
+	if(upper > length) {
+	    upper = length;
 	}
-    }
 
-    barrier(CLK_LOCAL_MEM_FENCE);
+	l_result[lId] = (int)-1;
 
-    if(lId == (int)0) {
-	g_result[0] = -1;
-	for(int i = 0; i < l_size; ++i) {
-	    if(l_result[i] >= 0) {
-		g_result[0] = l_result[i];
+	for(int gId = lower; gId < upper; gId++) {
+	    VALUE mine = data[gId];
+	    if(fabs(mine - value) < epsilon) {
+		l_result[lId] = lId;
 		break;
 	    }
 	}
-    }
+
+	barrier(CLK_LOCAL_MEM_FENCE);
+
+	if(lId == (int)0) {
+	    result[0] = (int)-1;
+	    for(int i = 0; i < l_size; ++i) {
+		if(l_result[i] >= 0) {
+		    result[0] = l_result[i];
+		    break;
+		}
+	    }
+	}
 }
 
--- ps6_search/search_ocl.c
+++ ps6_search/search_ocl.c
@@ -1,11 +1,11 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <malloc.h>
+#include <assert.h>
 #include <time.h>
-#include <math.h>
-#ifndef WIN32
 #include <stdbool.h>
-#endif
+#include <math.h>
 
 #include "cl_utils.h"
 #include "time_ms.h"
@@ -14,28 +14,8 @@
 #ifndef CL_DEVICE
 	#define CL_DEVICE 0
 #endif
-
-#ifndef LOC_SZ
-	#define LOC_SZ 1000
-#endif
-
+#define VALUE float
 #define KERNEL_FILE_NAME "./search.cl"
-#define VALUE double
-
-
-cl_uint getDurationMS(cl_event event) {
-	cl_ulong start = 0;
-	cl_ulong end = 0;
-	cl_int ret;
-
-	ret = clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_START, sizeof(cl_ulong), (void *)&start, NULL);
-	ret |= clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_END, sizeof(cl_ulong), (void *)&end, NULL);
-
-	CLU_ERRCHECK(ret, "Unable to read profiling information!");
-
-	// the values are measured in nano seconds!
-	return (end - start) / 1000000;
-}
 
 bool find(VALUE *data, VALUE val, VALUE epsilon, int N) {
 	for(int j=0; j<N; ++j) {
@@ -48,49 +28,52 @@ bool find(VALUE *data, VALUE val, VALUE epsilon, int N) {
 
 int main(int argc, char **argv)
 {	
+  
 	if(argc != 3) {
 		printf("Usage: search [elements] [iterations]\nExample: search 1000000 1000\n");
 		return -1;
 	}
 
 	int elems = atoi(argv[1]), iters = atoi(argv[2]);
-
-	// allocate memory for data set
-	VALUE *data = (VALUE*)malloc(elems*sizeof(VALUE));
-
+	
 	// initialize random number generator
 	dsfmt_t rand_state;
 	dsfmt_init_gen_rand(&rand_state, (uint32_t)time(NULL));
 
 	// initialize data set (fill randomly)
+	VALUE data[elems];
 	for(int j=0; j<elems; ++j) {
 		data[j] = dsfmt_genrand_close1_open2(&rand_state);
 	}
-
+	
+	//int elems = 100, iters = 1;
+	//int max_group_size = 100;
+	//int group_size = max_group_size;
+	
 	// ocl initialization
 	cl_context context;
 	cl_command_queue command_queue;
 	cl_device_id device_id = cluInitDevice(CL_DEVICE, &context, &command_queue);
-
+	
 	// compute local and global size based on maximum group size
 	// and number of elements
 	int max_group_size = cluGetMaxWorkGroupSize(device_id);
-	int local_size = max_group_size;
+	int group_size = max_group_size;
 	int global_size = elems;
-
+	
 	if(elems > max_group_size) {
 		global_size = max_group_size;
 		while(global_size < elems) {
 			global_size += max_group_size;
 		}
 	} else {
-		local_size = elems;
+		group_size = elems;
 	}
 
 	printf("Input size : %d\n", elems);
-	printf("Local size : %d\n", local_size);
+	printf("Local size : %d\n", group_size);
 	printf("Global size: %d\n", global_size);
-
+	
 	// create memory buffers
 	cl_int err;
 	cl_mem mem_data = clCreateBuffer(context, CL_MEM_READ_ONLY, elems * sizeof(VALUE), NULL, &err);
@@ -98,10 +81,8 @@ int main(int argc, char **argv)
 	CLU_ERRCHECK(err, "Failed to create buffer for matrix");
 
 	// fill memory buffers
-	int res = 0;
 	err = clEnqueueWriteBuffer(command_queue, mem_data, CL_FALSE, 0, elems * sizeof(VALUE), data, 0, NULL, NULL);
-	err = clEnqueueWriteBuffer(command_queue, mem_result, CL_FALSE, 0, sizeof(int), &res, 0, NULL, NULL);
-	CLU_ERRCHECK(err, "Failed to write data to device");
+	CLU_ERRCHECK(err, "Failed to write matrix to device");
 
 	// create kernel from source
 	char tmp[1024];
@@ -110,41 +91,44 @@ int main(int argc, char **argv)
 	cl_kernel kernel = clCreateKernel(program, "search", &err);
 	CLU_ERRCHECK(err, "Failed to create 'search' kernel from program");
 
+	VALUE value = 0;
+	
 	VALUE epsilon = 0.4/(VALUE)elems;
 	unsigned long long total_time = 0, total_found = 0, found_classical = 0;
-
-	for(int i=0; i<iters; ++i) {
+	
+	size_t glob_size[1] = {global_size};
+	size_t loc_size[1] = {group_size};
+	
+	for(int i = 0; i < iters; ++i) {
 		// search
 		unsigned long long start_time = time_ms();
+		//VALUE val = (VALUE)100;
 		VALUE val = (VALUE)dsfmt_genrand_close1_open2(&rand_state);
-		int result = -1;
-
+		
 		// set arguments and execute kernel
-		size_t g_size[] = {elems};
-		size_t l_size[] = {LOC_SZ};
 		cluSetKernelArguments(kernel, 6,
-							  sizeof(cl_mem), (void *)&mem_data,
-							  sizeof(cl_mem), (void *)&mem_result,
-							  sizeof(VALUE), (void *)&epsilon,
-							  sizeof(VALUE), (void *)&val,
-							  sizeof(int), (void *)&elems,
-							  local_size*sizeof(int), NULL);
+				      sizeof(cl_mem), (void *)&mem_data,
+				      sizeof(cl_mem), (void *)&mem_result,
+				      sizeof(VALUE), (void *)&epsilon,
+				      sizeof(VALUE), (void *)&value,
+				      sizeof(int), (void *)&elems,
+				      group_size*sizeof(int), NULL);
+				      
+		CLU_ERRCHECK(clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, glob_size, loc_size, 0, NULL, NULL), "Failed to enqueue 2D kernel");
 
-		err = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, g_size, l_size, 0, NULL, NULL);
-		CLU_ERRCHECK(err, "Failed to enqueue 2D kernel");
-
-		// copy results back to host
+		// read result
+		int result;
 		err = clEnqueueReadBuffer(command_queue, mem_result, CL_TRUE, 0, sizeof(int), &result, 0, NULL, NULL);
-		CLU_ERRCHECK(err, "Failed reading back result");
-		// check the result
+		CLU_ERRCHECK(err, "Failed to read result");
+		
+		total_time += time_ms() - start_time;
+		//printf("Result: %d\n", result);
+		
 		if(result >= 0)
 			total_found++;
 
-		total_time += time_ms() - start_time;
-
 		if(find(data, val, epsilon, elems))
 			found_classical++;
-
 	}
 
 	// print result
@@ -152,9 +136,7 @@ int main(int argc, char **argv)
 	printf("Maximum workgroup size: %d\n", cluGetMaxWorkGroupSize((device_id)));
 	printf("Done, took %12llu ms, found %12llu\n", total_time, total_found);
 	printf("Found with classical method: %llu\n", found_classical);
-
-	free(data);
-
+	
 	// finalization
 	err = clFinish(command_queue);
 	err |= clReleaseKernel(kernel);
@@ -163,7 +145,6 @@ int main(int argc, char **argv)
 	err |= clReleaseMemObject(mem_result);
 	err |= clReleaseCommandQueue(command_queue);
 	err |= clReleaseContext(context);
-
 	CLU_ERRCHECK(err, "Failed during ocl cleanup");
 	
 	return EXIT_SUCCESS;
